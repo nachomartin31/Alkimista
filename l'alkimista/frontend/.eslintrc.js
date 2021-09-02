47ced68e29d19da6f231425b78752a5c85db54ad
Nacho Martín <martin.nacho.31@gmail.com>
2021-09-02 18:47:22 +0200

    Change rule quotes to double quoting

--- l'alkimista/frontend/.eslintrc.js
+++ l'alkimista/frontend/.eslintrc.js
@@ -5,29 +5,29 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
-    'airbnb-base',
+    "plugin:vue/essential",
+    "airbnb-base",
   ],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaVersion: 2018,
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
   },
   plugins: [
-    'vue',
-    '@typescript-eslint',
+    "vue",
+    "@typescript-eslint",
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'linebreak-style': 0,
-    'global-require': 0,
-    'eslint linebreak-style': [0, 'error', 'windows'],
-    quotes: [1, 'single', 'avoid-escape'],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "linebreak-style": 0,
+    "global-require": 0,
+    "eslint linebreak-style": [0, "error", "windows"],
+    quotes: [2, "double", "avoid-escape"],
   },
 
 };
