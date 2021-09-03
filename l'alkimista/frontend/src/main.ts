7b40759e31fc554c1612939eb1b262eadf297b02
Nacho Martín <martin.nacho.31@gmail.com>
2021-09-03 17:19:09 +0200

    Change quotes to double

--- l'alkimista/frontend/src/main.ts
+++ l'alkimista/frontend/src/main.ts
@@ -1,8 +1,8 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(store).use(router).mount("#app");
