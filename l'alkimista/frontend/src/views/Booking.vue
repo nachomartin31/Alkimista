646dc7cd4c1608df7884011ea25780ab7cae3fb9
Nacho Martín <martin.nacho.31@gmail.com>
2021-09-04 15:01:00 +0200

    Add booking form

--- l'alkimista/frontend/src/views/Booking.vue
+++ l'alkimista/frontend/src/views/Booking.vue
@@ -1,3 +1,30 @@
 <template>
-  <h2>Reservar</h2>
+  <div>
+    <h2>Reservar</h2>
+    <form action="">
+      <label>Nom</label>
+      <input type="text" placeholder="Nom" />
+      <label>Dia</label>
+      <input type="Date" placeholder="Dia" />
+      <label>Servei</label>
+      <select placeholder="Servei">
+        <option value="Migdia">Migdia</option>
+        <option value="Nit">Nit</option>
+      </select>
+      <label for="">Torn</label>
+      <select placeholder="Torn">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+      </select>
+    </form>
+  </div>
 </template>
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  width: 12rem;
+}
+</style>
