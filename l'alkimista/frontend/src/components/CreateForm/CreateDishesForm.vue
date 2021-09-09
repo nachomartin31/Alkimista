59de9a3edcca24062fcc338d3633ce6544b59be3
Nacho Martín <martin.nacho.31@gmail.com>
2021-09-09 13:35:34 +0200

    Add data

--- l'alkimista/frontend/src/components/CreateForm/CreateDishesForm.vue
+++ l'alkimista/frontend/src/components/CreateForm/CreateDishesForm.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
     <div>
+      <label for="name">Name:</label>
+      <input type="text" name="name" v-model="name" />
+    </div>
+    <div>
       <label for="category">Category:</label>
-      <input type="text" name="category" />
+      <input type="text" name="category" v-model="category" />
     </div>
     <div>
       <label for="image">Image:</label>
@@ -10,32 +14,67 @@
     </div>
     <div>
       <label for="descriptionCat">Description in catalan:</label>
-      <input type="text" name="descriptionCat" />
+      <input type="text" name="descriptionCat" v-model="descriptionCat" />
     </div>
     <div>
       <label for="descriptionSpa">Description in spanish:</label>
-      <input type="text" name="descriptionSpa" />
+      <input type="text" name="descriptionSpa" v-model="descriptionSpa" />
     </div>
     <div>
       <label for="ingredientsCat">Ingredients in catalan:</label>
-      <input type="text" name="ingredentsCat" />
+      <input type="text" name="ingredentsCat" v-model="ingredientsCat" />
     </div>
     <div>
       <label for="ingredientsSpa">Ingredients in spanish:</label>
-      <input type="text" name="ingredentsSpa" />
+      <input type="text" name="ingredentsSpa" v-model="ingredientsSpa" />
     </div>
     <div>
       <label for="tags">Preferences:</label>
       <div>
         <label for="tags">Glutten-free</label>
-        <input type="checkbox" />
+        <input type="checkbox" name="Glutten" />
         <label for="tags">Proximity</label>
-        <input type="checkbox" />
+        <input type="checkbox" name="Proximity" />
         <label for="tags">Vegan</label>
-        <input type="checkbox" />
+        <input type="checkbox" name="Vegan" />
       </div>
     </div>
     <label for="price">Price</label>
-    <input type="text" />
+    <input type="text" v-model="price" />
+    <button type="button" @click="printData">Confirm</button>
   </div>
 </template>
+<script>
+export default {
+  data: () => ({
+    name: "",
+    category: "",
+    image: "",
+    descriptionCat: "",
+    descriptionSpa: "",
+    ingredientsCat: "",
+    ingredientsSpa: "",
+    tags: [],
+    price: "",
+  }),
+  methods: {
+    createDishesObjectToSend() {
+      const data = {
+        name: this.name,
+        category: this.category,
+        image: this.image,
+        descriptionCat: this.descriptionCat,
+        descriptionSpa: this.descriptionSpa,
+        ingredientsCat: this.ingredientsCat,
+        ingredientsSpa: this.ingredientsSpa,
+        tags: this.tags,
+        price: parseFloat(this.price),
+      };
+      return data;
+    },
+    printData() {
+      console.log(this.createDishesObjectToSend());
+    },
+  },
+};
+</script>
